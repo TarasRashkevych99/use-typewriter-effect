6d73a711a219611009a08ffa4f634bb13b8eeb5f
Taras Rashkevych <taras.rashkevych1999@gmail.com>
2025-09-07 23:31:43 +0200

    add: composable for typewriter effect

---
+++ src/App.vue
@@ -0,0 +1,68 @@
+<script setup>
+import { useTypewriterEffect } from './composables/useTypewriterEffect';
+import { watch } from 'vue';
+
+const {
+  text,
+  isWriting,
+  write,
+  reset,
+  stop,
+  resume } = useTypewriterEffect({
+    speed: 10,
+    numberOfSymbols: 1.5
+  })
+
+function handlerWrite() {
+  write('Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.')
+}
+
+watch(isWriting, (newValue, oldValue) => {
+  console.log(`Do something when the isWriting variable changes from ${newValue} to ${oldValue}`)
+})
+
+</script>
+
+<template>
+  <div class="container">
+    <div class="buttons">
+      <button @click="handlerWrite">
+        Typewriter effect
+      </button>
+      <button @click="reset">
+        Reset
+      </button>
+      <button @click="stop">
+        Stop
+      </button>
+      <button @click="resume">
+        Resume
+      </button>
+    </div>
+    <div class="typewriter-text-wrapped">
+    {{ text }}
+    </div>
+    <div>
+    Is it writing? {{ isWriting }}
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  width: 500px;
+}
+
+.buttons {
+  display: flex;
+  column-gap: 0.5rem;
+  margin-bottom: 0.5rem;
+}
+
+/* .typewriter-text-wrapped {
+  height: 100px;
+  overflow-y: auto;
+} */
+</style>
---
+++ src/composables/useTypewriterEffect.js
@@ -0,0 +1,106 @@
+import { ref, readonly } from 'vue'
+
+export function useTypewriterEffect(options) {
+    const _text = ref('')
+    const isWriting = ref(false)
+    let _inputText = ''
+    let _currentIndex = 0
+    let _interval = -1
+    let _stopped = false
+    let _resuming = false
+    let _options = _setOptions(options)
+
+    function write(inputText) {
+        if (_interval > 0 && !_resuming) {
+            console.warn('the typewritter effect is already active, call reset() to reset the state')
+            return
+        }
+
+        _ifNotStringThrow(inputText)
+
+        _inputText = inputText
+        _interval = setInterval(() => {
+            if (_currentIndex < _inputText.length) {
+                isWriting.value = true
+                if (_options.numberOfSymbols > 1) {
+                    _text.value += _inputText.slice(_currentIndex, _currentIndex + _options.numberOfSymbols)
+                } else {
+                    _text.value += _inputText.at(_currentIndex)
+                }
+                _currentIndex += _options.numberOfSymbols
+            }
+            else {
+                clearInterval(_interval)
+                isWriting.value = false
+            }
+        }, _options.speed)
+    }
+
+    function reset() {
+        stop()
+        _text.value = ''
+        _inputText = ''
+        _currentIndex = 0
+        _interval = -1
+        _stopped = false
+        _resuming = false
+    }
+
+    function stop() {
+        _stopped = true
+        if (_interval > 0) {
+            clearInterval(_interval)
+            isWriting.value = false
+        }
+    }
+
+    function resume() {
+        if (_stopped && _inputText) {
+            _stopped = false
+            _resuming = true
+            write(_inputText)
+            _resuming = false
+        }
+    }
+
+    function _setOptions(inputOptions) {
+        console.log("Input options", inputOptions)
+
+        let defaultOptions = {
+            speed: 10,
+            numberOfSymbols: 1
+        }
+
+        if (inputOptions.speed && typeof inputOptions.speed === 'number' && inputOptions.speed > 0) {
+            defaultOptions.speed = inputOptions.speed
+        } else if (inputOptions.speed) {
+            console.warn(`the speed option should be a valid number greater then 0. The defualt value ${defaultOptions.speed} will be used`)
+        }
+        if (inputOptions.numberOfSymbols && typeof inputOptions.numberOfSymbols === 'number' && inputOptions.numberOfSymbols >= 1) {
+            defaultOptions.numberOfSymbols = Math.ceil(inputOptions.numberOfSymbols)
+        } else if (inputOptions.numberOfSymbols) {
+            console.warn(`the numberOfSymbols option should be a valid number greater or equal to 1. The defualt value ${defaultOptions.numberOfSymbols} will be used`)
+        }
+
+        console.log("Final options", defaultOptions)
+
+        return defaultOptions
+    }
+
+    function _ifNotStringThrow(inputText) {
+        if (!inputText || typeof inputText !== "string") {
+            throw new Error('the type of the text must be string')
+        }
+    }
+
+    const text = readonly(_text)
+
+    return {
+        text,
+        isWriting,
+        write,
+        reset,
+        stop,
+        resume
+    }
+}
